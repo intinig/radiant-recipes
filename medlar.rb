d36fb8ed968888f07f5465e3d2bae7ad5fe40153
intinig <intinig@6302a142-54df-4d79-849e-df2f1e54eb4f>
2007-09-19 13:15:49 +0000

    medlar recipes
    git-svn-id: https://svn1.hosted-projects.com/medlar/recipes/trunk@2 6302a142-54df-4d79-849e-df2f1e54eb4f

---
+++ medlar.rb
@@ -0,0 +1,31 @@
+# Keep it _FAST_
+set :deploy_via, :remote_cache
+
+# Default medlar repository
+set :repository,  "https://svn1.hosted-projects.com/medlar/#{application}/trunk"
+
+# SCM information
+set :scm_username, "intinig"
+set :scm_password, Proc.new { CLI.password_prompt "SVN Password: "}
+
+namespace :deploy do
+  after "deploy:setup", "deploy:medlar:freeze_rails"
+  after "deploy:update", "deploy:medlar:link_rails"
+  
+  namespace :medlar do
+    desc "Fetch Rails1.2 stable and puts it into shared."
+    task :freeze_rails do
+      run "cd #{shared_path}; rm -rf rails; svn co http://svn.rubyonrails.org/rails/branches/1-2-stable rails"
+    end
+    
+    desc "Updates Rails1.2 stable to latest version."
+    task :update_rails do
+      run "cd #{shared_path}/rails; svn up"
+    end
+    
+    desc "Links Rails1.2 to application."
+    task :link_rails do
+      run "cd #{current_path}/vendor; rm -rf rails; ln -s #{shared_path}/rails ./rails"
+    end
+  end
+end
---
+++ site5.rb
@@ -0,0 +1,36 @@
+# Necessary to run on Site5
+set :use_sudo, false
+set :group_writable, false
+
+# Less releases, less space wasted
+set :keep_releases, 2
+
+# This is related to site5 too.
+set :deploy_to, "/home/#{user}/apps/#{application}"
+
+namespace :deploy do
+  after "deploy:update", "deploy:site5:link_public_html"
+    
+  desc <<-DESC
+    Site5 version of restart task.
+  DESC
+  task :restart do
+    site5::kill_dispatch_fcgi
+  end
+  
+  namespace :site5 do
+    desc <<-DESC
+      Links public_html to current_path/public
+    DESC
+    task :link_public_html do
+      run "cd /home/#{user}; rm -rf public_html; ln -s #{current_path}/public ./public_html"
+    end
+    
+    desc <<-DESC
+      Kills Ruby instances on Site5
+    DESC
+    task :kill_dispatch_fcgi do
+      run "skill -u #{user} -c ruby"
+    end
+  end
+end
