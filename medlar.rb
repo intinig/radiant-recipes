72973a10445fa981acac93ed3e301b886dc6cce5
intinig <intinig@6302a142-54df-4d79-849e-df2f1e54eb4f>
2007-10-08 09:52:21 +0000

    Added a tail_logs task
    git-svn-id: https://svn1.hosted-projects.com/medlar/recipes/trunk@18 6302a142-54df-4d79-849e-df2f1e54eb4f

--- medlar.rb
+++ medlar.rb
@@ -41,6 +41,15 @@ namespace :deploy do
 
     end
     
+    # Blatantly stolen from: http://errtheblog.com/post/21
+    desc "tail production log files" 
+    task :tail_logs, :roles => :app do
+      run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+        puts  # for an extra line break before the host name
+        puts "#{channel[:host]}: #{data}" 
+        break if stream == :err    
+      end
+    end
   end
 
 end
