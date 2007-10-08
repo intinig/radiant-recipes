8d7a14aa0a2d6a0b3432883c520479b675e91990
intinig <intinig@6302a142-54df-4d79-849e-df2f1e54eb4f>
2007-10-08 07:36:26 +0000

    git-svn-id: https://svn1.hosted-projects.com/medlar/recipes/trunk@15 6302a142-54df-4d79-849e-df2f1e54eb4f

--- radiant.rb
+++ radiant.rb
@@ -1,6 +1,7 @@
 namespace :deploy do
   after "deploy:cold", "deploy:radiant:bootstrap"
   after "deploy:migrate", "deploy:radiant:migrate:extensions"
+  
   desc "Overridden deploy:cold for Radiant."
   task :cold do
     update
